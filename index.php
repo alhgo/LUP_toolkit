28230a88888248d5d54309c513718766b0784a87
alhgo <correo@laultimapregunta.com>
2018-10-12 17:44:36 +0200

    First Commit
    Se ha eliminado la carpeta "giigsey" de la librería Firebase PHP

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('includes/toolkit.php');
+
+//Obtenemos los datos del sitio y del usuario
+$user = new Users;
+$site = new Site;
+
+?>
+
+<?php snippet('header.php', ['site' => $site]); ?>
+
+<body>
+
+<?php snippet('nav.php',['menu' => array('Menú 1' => 'menu1.html', 'Menú 2' => 'menu2.html'), 'site' => $site, 'user' => $user]); ?>
+
+	<div class="container-fluid p-0 m-0">
+		
+		<?php snippet('breadcrumb.php',array('data' => ['Inicio' => 'index.php'])); ?>
+		
+		<?php snippet('map.php'); ?>
+
+		<?php if(!$user->logged) snippet('login_form_modal.php'); ?>
+
+	</div>
+	
+<?php snippet('footer.php', ['libs' => array('forms.js')]); ?>
+
+</body>
+</html>
